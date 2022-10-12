3c150ef451e1aa96a609fc51af994887a1abb897
Idan Yaffe <idan.yaffe@insify.io>
2022-10-12 16:03:39 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -995,7 +995,7 @@ exports.getBooleanInput = getBooleanInput;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 function setOutput(name, value) {
     process.stdout.write(os.EOL);
-    command_1.issueCommand('set-output', { name }, value);;
+    command_1.issueCommand('set-output', { name }, value);
 }
 exports.setOutput = setOutput;
 /**
